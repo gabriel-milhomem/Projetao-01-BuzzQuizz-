daacd41f4a970c79fa47fa342f5ced4193ac0a31
Gabriel Milhomem <gabriell.mil@gmail.com>
2020-09-25 01:12:14 -0300

    InterfaceQuizz: Esperar 2 segundos & Cores Fundo & Aleatoreamente

--- assets/javascript/iniciarJogo.js
+++ assets/javascript/iniciarJogo.js
@@ -3,6 +3,11 @@ var ind = 0;
 var qualNivel = 0;
 var quizz;
 var interfaceFinal;
+var resposta;
+var listaRespostas;
+// var acertos = 0;
+var score;
+// var totalPerguntas;
 
 function iniciarJogo(id) {
     interfaceJogo = document.querySelector("#interfaceQuizz");
@@ -18,29 +23,71 @@ function iniciarJogo(id) {
 function renderizarTelaJogo() {
     var titulo = document.querySelector("#interfaceQuizz h1");
     var textoPergunta = document.querySelector("#interfaceQuizz h2");
-    var resposta = document.querySelectorAll("#interfaceQuizz figcaption");
+    resposta = document.querySelectorAll("#interfaceQuizz figcaption");
     var imagens = document.querySelectorAll("#interfaceQuizz figure");
     var perguntas = quizz.data.perguntas[ind];
+    listaRespostas = perguntas.respostas.sort(comparador);
 
     titulo.innerText = quizz.title;
     textoPergunta.innerHTML = "<span>" + (ind + 1) + ". </span>" + perguntas.titulo;
     for(var j = 0; j < 4; j++) {
-        resposta[j].innerHTML = "<p>" + perguntas.respostas[j].texto + "</p>";
-        imagens[j].innerHTML = "<img src= '" + perguntas.respostas[j].link + "'/>";
+        resposta[j].innerHTML = "<p>" + listaRespostas[j].texto + "</p>";
+        imagens[j].innerHTML = "<img alt= '" + listaRespostas[j].link + "'/>";
     }
 
     ind++;
     console.log(quizz);
 }
 
-function selecionarOpcao() {
-    if(ind < quizz.data.perguntas.length) {
-        renderizarTelaJogo();
+function selecionarOpcao(cartaSelecionada) {
+    totalPerguntas = quizz.data.perguntas.length;
+    corNoFundo();
+    interarAcerto(cartaSelecionada);
+    
+    if(ind < totalPerguntas) {
+        setTimeout(resetarFundo, 2000);
     }
 
     else {
-        iniciarInterfaceFinal();
+        //qualNivelEh();
+        setTimeout(iniciarInterfaceFinal, 1000);
     }
+}
+
+function interarAcerto(selecionada) {
+    if(listaRespostas[selecionada].ehResp === true) {
+        acertos++;
+    }
+}
+
+/*function qualNivelEh() {
+    score = Math.round(acertos / totalPerguntas); 
+}*/
+
+function corNoFundo(carta) {
+    for(var j = 0; j < 4; j++) {
+        if(listaRespostas[j].ehResp === true) {
+            resposta[j].classList.add("fundoVerde");
+        }
+
+        else {
+            resposta[j].classList.add("fundoVerm");
+        }
+    }
+}
+
+function resetarFundo() {
+    for(var j = 0; j < 4; j++) {
+        if(listaRespostas[j].ehResp === true) {
+            resposta[j].classList.remove("fundoVerde");
+        }
+
+        else {
+            resposta[j].classList.remove("fundoVerm");
+        }
+    }
+    
+    renderizarTelaJogo();
 }
 
 function iniciarInterfaceFinal() {
@@ -53,17 +100,24 @@ function renderizarTelaFinal() {
     var titulo = document.querySelector("#interfaceFinal h1");
     var tituloNivel = document.querySelector("#interfaceFinal h3");
     var descricao = document.querySelector("#interfaceFinal p");
-    var listaSpans = document.querySelectorAll(".cabecalhoArticle span");
+    var listaSpans = document.querySelectorAll("#resultadoQuizz span");
     var imagem = document.querySelector("#interfaceFinal figure");
-    //var acertos = listaSpans[0];
-    var totalPerguntas = listaSpans[1];
-    //var score = listaSpans[2];
+    var totalAcertos = listaSpans[0];
+    var totPergunta = listaSpans[1];
+    var resulScore = listaSpans[2];
     var niveis = quizz.data.niveis[qualNivel];
 
-    
-    totalPerguntas.innerText = quizz.data.perguntas.length;
-    titulo.innerText = quizz.title
-    imagem.innerHTML = "<img src='" + niveis.linkNivel + "'/>"
+    console.log(listaSpans);
+    console.log("Acertos", acertos);
+    // totalAcertos.innerText = acertos;
+    console.log("Perguntas", totalPerguntas);
+    totPergunta.innerText = totalPerguntas;
+    titulo.innerText = quizz.title;
+    imagem.innerHTML = "<img alt='" + niveis.linkNivel + "'/>";
     tituloNivel.innerText = niveis.tituloNivel;
     descricao.innerText = niveis.descri;
+}
+
+function comparador() { 
+    return Math.random() - 0.5;
 }
